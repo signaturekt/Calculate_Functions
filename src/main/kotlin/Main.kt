c3e83f91d1b27ade60473c26f4c4b6339f37c424
signaturekt <kevin97truong@gmail.com>
2023-10-08 18:56:21 -0700

    Add calculations and print to screen

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
-    val num1 = 3;
-    val num2 = 6;
-    val num3 = 9;
+    val num1 = 3
+    val num2 = 6
+    val num3 = 9
 
     println("The values I'm working with are: ${num1}, ${num2}, and ${num3}")
     println("The sum is: ${sum(num1, num2, num3)}")
@@ -12,19 +12,19 @@ fun main(args: Array<String>) {
 }
 
 fun sum(x: Int, y: Int, z: Int): Int {
-    return x + y + z;
+    return x + y + z
 }
 
 fun average(x: Int, y: Int, z: Int): Float {
-    return sum(x, y, z)/3f;
+    return sum(x, y, z)/3f
 }
 
 fun product(x: Int, y: Int, z: Int): Int {
-    return x * y * z;
+    return x * y * z
 }
 
 fun smallest(x: Int, y: Int, z: Int): Int {
-    var smallestNum = x;
+    var smallestNum = x
     if(x > y) {
         smallestNum = y
     }
@@ -35,7 +35,7 @@ fun smallest(x: Int, y: Int, z: Int): Int {
 }
 
 fun largest(x: Int, y: Int, z: Int): Int {
-    var largestNum = x;
+    var largestNum = x
     if(x < y) {
         largestNum = y
     }
