c4061a771ce6d81c9e8d0c0dc1ee4534ffd53808
signaturekt <kevin97truong@gmail.com>
2023-10-08 18:55:45 -0700

    Add calculations and print to screen

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,46 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val num1 = 3;
+    val num2 = 6;
+    val num3 = 9;
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println("The values I'm working with are: ${num1}, ${num2}, and ${num3}")
+    println("The sum is: ${sum(num1, num2, num3)}")
+    println("The average is: ${average(num1, num2, num3)}")
+    println("The product is: ${product(num1, num2, num3)}")
+    println("The smallest is: ${smallest(num1, num2, num3)}")
+    println("The largest is: ${largest(num1, num2, num3)}")
+}
+
+fun sum(x: Int, y: Int, z: Int): Int {
+    return x + y + z;
+}
+
+fun average(x: Int, y: Int, z: Int): Float {
+    return sum(x, y, z)/3f;
+}
+
+fun product(x: Int, y: Int, z: Int): Int {
+    return x * y * z;
+}
+
+fun smallest(x: Int, y: Int, z: Int): Int {
+    var smallestNum = x;
+    if(x > y) {
+        smallestNum = y
+    }
+    if(y > z) {
+        smallestNum = z
+    }
+    return smallestNum;
+}
+
+fun largest(x: Int, y: Int, z: Int): Int {
+    var largestNum = x;
+    if(x < y) {
+        largestNum = y
+    }
+    if(y < z) {
+        largestNum = z
+    }
+    return largestNum;
 }
